c255d295c49aadf9e4a5857032be691a58093688
Ezeiru-Elias Chukwubuikem Emma <105207318+Ezeiruemma@users.noreply.github.com>
2023-06-29 02:12:05 +0100

    made chages on task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
-export default function getRespondFromAPI() {
-    return new Promise((resolve, reject) => {
-      if (true) {
-        resolve();
-      } else {
-        reject();
-      }
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (!success) reject(new Error('The fake API is not working currently'));
+    resolve({
+      status: 200,
+      body: 'Success',
     });
-  }
+  });
+}
