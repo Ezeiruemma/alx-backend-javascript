9167c8cbc0e0715917f7991e1285cdf591944cd8
Ezeiru-Elias Chukwubuikem Emma <105207318+Ezeiruemma@users.noreply.github.com>
2023-07-11 03:12:23 +0100

    more depedencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,32 @@
-{
-  "env": {
-      "browser": true,
-      "es2021": true
-  },
-  "extends": [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:react/recommended"
-  ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-      "ecmaVersion": "latest",
-      "sourceType": "module"
-  },
-  "plugins": [
-      "@typescript-eslint",
-      "react"
-  ],
-  "rules": {
-  }
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:vue/vue3-essential"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
 }
